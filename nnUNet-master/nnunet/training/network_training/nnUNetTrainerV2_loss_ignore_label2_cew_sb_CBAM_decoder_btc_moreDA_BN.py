bb1b7aaf80b95fad2b29c6588c1d5bf21676cf75
Pattern Recognition and Bioimage Informatics Group <84379335+PRBioimages@users.noreply.github.com>
2023-12-20 11:17:45 +0800

    Add files via upload

---
+++ nnUNet-master/nnunet/training/network_training/nnUNetTrainerV2_loss_ignore_label2_cew_sb_CBAM_decoder_btc_moreDA_BN.py
@@ -0,0 +1,67 @@
+from nnunet.training.network_training.nnUNetTrainerV2_loss_ignore_label2_ce_w_sb import nnUNetTrainerV2_loss_ignore_label2_ce_w_sb
+from nnunet.network_architecture.generic_UNet import Generic_UNet
+from nnunet.network_architecture.initialization import InitWeights_He
+from torch import nn
+import torch
+from nnunet.utilities.nd_softmax import softmax_helper
+
+class nnUNetTrainerV2_loss_ignore_label2_cew_sb_CBAM_decoder_btc_moreDA_BN(nnUNetTrainerV2_loss_ignore_label2_ce_w_sb): # BL + SE
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+
+    def setup_DA_params(self):
+        super().setup_DA_params()
+        self.data_aug_params['selected_seg_channels'] = [0]
+        self.data_aug_params['patch_size_for_spatialtransform'] = self.patch_size
+
+        self.data_aug_params["p_rot"] = 0.3
+
+        self.data_aug_params["scale_range"] = (0.65, 1.6)
+        self.data_aug_params["p_scale"] = 0.3
+
+        self.data_aug_params["do_elastic"] = True
+        self.data_aug_params["p_eldef"] = 0.3
+
+        self.data_aug_params["do_additive_brightness"] = True
+        self.data_aug_params["additive_brightness_mu"] = 0
+        self.data_aug_params["additive_brightness_sigma"] = 0.2
+        self.data_aug_params["additive_brightness_p_per_sample"] = 0.3
+        self.data_aug_params["additive_brightness_p_per_channel"] = 1
+
+        self.data_aug_params['gamma_range'] = (0.5, 1.6)
+
+        self.data_aug_params['num_cached_per_thread'] = 4
+    def initialize_network(self):
+        """inference_apply_nonlin to sigmoid + larger unet"""
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            # norm_op = nn.InstanceNorm3d
+            norm_op = nn.BatchNorm3d
+
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            # norm_op = nn.InstanceNorm3d
+            norm_op = nn.BatchNorm3d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        self.network = Generic_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                    len(self.net_num_pool_op_kernel_sizes),
+                                    self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
+                                    dropout_op_kwargs,
+                                    net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True, 320,
+                                    ifCBAM_decoder_before_tc=True)
+        # print(self.network)
+        # if torch.cuda.is_available():
+        #     self.network.cuda()
+        # self.network.inference_apply_nonlin = nn.Sigmoid()
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = softmax_helper
---
+++ nnUNet-master/nnunet/training/network_training/nnUNetTrainerV2_loss_ignore_label2_sb.py
@@ -0,0 +1,52 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
+from nnunet.training.loss_functions.dice_loss import SoftDiceLoss
+from nnunet.utilities.nd_softmax import softmax_helper
+from nnunet.training.loss_functions.deep_supervision import MultipleOutputLoss2
+from nnunet.training.loss_functions.dice_loss import DC_and_CE_loss
+from nnunet.training.dataloading.dataset_loading import load_dataset, DataLoader3D, DataLoader2D, unpack_dataset
+
+
+class nnUNetTrainerV2_loss_ignore_label2_sb(nnUNetTrainerV2):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.loss = DC_and_CE_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}, {},ignore_label=2)
+        # self.loss = DC_and_ICE_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}, {}, ignore_label=2)
+        # self.loss = DC_and_CE_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}, {})
+        self.save_every = 50
+        self.skip_blank=True
+    def get_basic_generators(self):
+        self.load_dataset()
+        self.do_split()
+
+        if self.threeD:
+            dl_tr = DataLoader3D(self.dataset_tr, self.basic_generator_patch_size, self.patch_size, self.batch_size,
+                                 False, oversample_foreground_percent=self.oversample_foreground_percent,skip_blank=self.skip_blank,
+                                 pad_mode="constant", pad_sides=self.pad_all_sides, memmap_mode='r')
+            dl_val = DataLoader3D(self.dataset_val, self.patch_size, self.patch_size, self.batch_size, False,
+                                  oversample_foreground_percent=self.oversample_foreground_percent,skip_blank=self.skip_blank,
+                                  pad_mode="constant", pad_sides=self.pad_all_sides, memmap_mode='r')
+        else:
+            dl_tr = DataLoader2D(self.dataset_tr, self.basic_generator_patch_size, self.patch_size, self.batch_size,
+                                 oversample_foreground_percent=self.oversample_foreground_percent,
+                                 pad_mode="constant", pad_sides=self.pad_all_sides, memmap_mode='r')
+            dl_val = DataLoader2D(self.dataset_val, self.patch_size, self.patch_size, self.batch_size,
+                                  oversample_foreground_percent=self.oversample_foreground_percent,
+                                  pad_mode="constant", pad_sides=self.pad_all_sides, memmap_mode='r')
+        return dl_tr, dl_val
